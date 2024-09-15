e461345a228d03dcb7a4ac29f02fb6afd81ec7b2
myduyen3103 <vtmyduyen3103@gmail.com>
2024-09-15 16:32:07 +0700

    fe theme

---
+++ JobTrans/src/java/jobtrans/config/Configuration.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package jobtrans.config;
+
+/**
+ *
+ * @author admin
+ */
+public interface Configuration {
+    public static String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static String url = "jdbc:sqlserver://127.0.0.1:1433;databaseName=Jobtrans;";
+    public static String user = "sa";
+    public static String pass = "1";
+    
+    public static String templatePath = null ;
+}

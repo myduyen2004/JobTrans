7e374d9493752b55a9487ae792162b27d89bb076
QuangND <nguyendinhquang25082004@gmail.com>
2024-09-17 10:41:06 +0700

    Model class

--- JobTrans/src/java/jobtrans/model/Job.java
+++ JobTrans/src/java/jobtrans/model/Job.java
@@ -4,10 +4,106 @@
  */
 package jobtrans.model;
 
+import java.util.Date;
+
 /**
  *
  * @author admin
  */
 public class Job {
+    private int jobId;
+    private int userId;
+    private String jobTitle;
+    private float budget;
+    private String description;
+    private Date dueDate;
+    private boolean status;
+    private int categoryId;
+    private String empFeedback;
+    private String seekerFeedback;
+
+    public Job() {
+    }
+
+    
+    public int getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(int jobId) {
+        this.jobId = jobId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public float getBudget() {
+        return budget;
+    }
+
+    public void setBudget(float budget) {
+        this.budget = budget;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getEmpFeedback() {
+        return empFeedback;
+    }
+
+    public void setEmpFeedback(String empFeedback) {
+        this.empFeedback = empFeedback;
+    }
+
+    public String getSeekerFeedback() {
+        return seekerFeedback;
+    }
+
+    public void setSeekerFeedback(String seekerFeedback) {
+        this.seekerFeedback = seekerFeedback;
+    }
     
 }
--- JobTrans/src/java/jobtrans/model/User.java
+++ JobTrans/src/java/jobtrans/model/User.java
@@ -9,5 +9,165 @@ package jobtrans.model;
  * @author admin
  */
 public class User {
+    private int user_id;
+    private String user_name;
+    private String email;
+    private String password;
+    private String oauth_provider;
+    private String oauth_id;
+    private String role;
+    private int balance;
+    private String description;
+    private String specification;
+    private String address;
+    private String avatar_url;
+    private boolean status;
+
+    // Default constructor
+    public User() {
+    }
+
+    // Parameterized constructor
+    public User(int user_id, String user_name, String email, String password, String oauth_provider, String oauth_id,
+                String role, int balance, String description, String specification, String address, String avatar_url, boolean status) {
+        this.user_id = user_id;
+        this.user_name = user_name;
+        this.email = email;
+        this.password = password;
+        this.oauth_provider = oauth_provider;
+        this.oauth_id = oauth_id;
+        this.role = role;
+        this.balance = balance;
+        this.description = description;
+        this.specification = specification;
+        this.address = address;
+        this.avatar_url = avatar_url;
+        this.status = status;
+    }
+
+    // Getters and Setters for each field
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getOauth_provider() {
+        return oauth_provider;
+    }
+
+    public void setOauth_provider(String oauth_provider) {
+        this.oauth_provider = oauth_provider;
+    }
+
+    public String getOauth_id() {
+        return oauth_id;
+    }
+
+    public void setOauth_id(String oauth_id) {
+        this.oauth_id = oauth_id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(String specification) {
+        this.specification = specification;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    // Overriding toString() method to display the User object
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", user_name='" + user_name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", oauth_provider='" + oauth_provider + '\'' +
+                ", oauth_id='" + oauth_id + '\'' +
+                ", role='" + role + '\'' +
+                ", balance=" + balance +
+                ", description='" + description + '\'' +
+                ", specification='" + specification + '\'' +
+                ", address='" + address + '\'' +
+                ", avatar_url='" + avatar_url + '\'' +
+                ", status=" + status +
+                '}';
+    }
     
 }

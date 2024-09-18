b8e0f3c3df70e79fcf17f9d49fdd68cd8430485b
myduyen3103 <vtmyduyen3103@gmail.com>
2024-09-18 07:16:28 +0700

    email-noti,login google

--- JobTrans/src/java/jobtrans/dal/UserDAO.java
+++ JobTrans/src/java/jobtrans/dal/UserDAO.java
@@ -4,10 +4,95 @@
  */
 package jobtrans.dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jobtrans.model.User;
+import jobtrans.utils.DBConnection;
+
 /**
  *
  * @author admin
  */
 public class UserDAO {
+    private final DBConnection dbConnection;
+
+    public UserDAO() {
+        dbConnection = DBConnection.getInstance();
+    }
     
+    public void addUserByLoginGoogle(User user){
+        String sql = "INSERT INTO User(user_name, email, oauth_provider, oauth_id, role, balance, status)"
+                + " VALUES (?,?,?,?,?,?,?)";
+        try {
+            Connection con = dbConnection.openConnection();
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setNString(1,user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getOauthProvider());
+            statement.setString(4, user.getOauthId());
+            statement.execute();
+            statement.close();
+            con.close();
+        } catch(Exception ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public User getUserByEmail(String email){
+        DBConnection db = DBConnection.getInstance();
+        User user = null;
+        String sql = "SELECT COUNT(*) FROM [dbo].[User] WHERE email = ?";
+        
+        try {
+            Connection con = db.openConnection();
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, email);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                int userId = rs.getInt(1);
+                String userName = rs.getNString(2);
+                String password = rs.getString(4);
+                String oauthProvider = rs.getString(5);
+                String oauthId = rs.getString(6);
+                String role = rs.getString(7);
+                double balance = rs.getInt(8);
+                String description = rs.getNString(9);
+                String specification = rs.getNString(10);
+                String address = rs.getNString(11);
+                String avatarUrl = rs.getNString(12);
+                boolean status = rs.getBoolean(13);
+                user = new User(userId, userName, email, password, oauthProvider, oauthId, role, userId, description, specification, address, avatarUrl, status);
+            }
+            rs.close();
+            statement.close();
+            con.close();
+        } catch (Exception ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return user;
+    }
+    
+    public boolean checkExistEmail(String email){
+        DBConnection db = DBConnection.getInstance();
+        User user = null;
+        String sql = "SELECT * FROM [dbo].[User] WHERE email = ?";
+        boolean check = false;
+        try {
+            Connection con = db.openConnection();
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, email);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()== true) {
+                check = true;
+            }
+            rs.close();
+            statement.close();
+            con.close();
+        } catch (Exception ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return check;
+    }
 }
--- JobTrans/src/java/jobtrans/model/User.java
+++ JobTrans/src/java/jobtrans/model/User.java
@@ -9,5 +9,148 @@ package jobtrans.model;
  * @author admin
  */
 public class User {
+    private int userId;
+    private String userName;
+    private String email;
+    private String password;
+    private String oauthProvider;
+    private String oauthId;
+    private String role;
+    private int balance;
+    private String description;
+    private String specification;
+    private String address;
+    private String avatarUrl;
+    private boolean status;
+
+    public User() {
+    }
+
+    public User(int userId, String userName, String email, String password, String oauthProvider, String oauthId, String role, int balance, String description, String specification, String address, String avatarUrl, boolean status) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.oauthProvider = oauthProvider;
+        this.oauthId = oauthId;
+        this.role = role;
+        this.balance = balance;
+        this.description = description;
+        this.specification = specification;
+        this.address = address;
+        this.avatarUrl = avatarUrl;
+        this.status = status;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getOauthProvider() {
+        return oauthProvider;
+    }
+
+    public void setOauthProvider(String oauthProvider) {
+        this.oauthProvider = oauthProvider;
+    }
+
+    public String getOauthId() {
+        return oauthId;
+    }
+
+    public void setOauthId(String oauthId) {
+        this.oauthId = oauthId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(String specification) {
+        this.specification = specification;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userId=" + userId + ", userName=" + userName + ", email=" + email + ", password=" + password + ", oauthProvider=" + oauthProvider + ", oauthId=" + oauthId + ", role=" + role + ", balance=" + balance + ", description=" + description + ", specification=" + specification + ", address=" + address + ", avatarUrl=" + avatarUrl + ", status=" + status + '}';
+    }
     
+    
+
 }
